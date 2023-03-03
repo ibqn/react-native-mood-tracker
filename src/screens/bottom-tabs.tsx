e936ec7cd0144189b5c2613fe9cd713fd32a5990
ibqn <evgen.ibqn@gmail.com>
2023-03-03 17:24:40 +0100

    include analytics to tabs

--- src/screens/bottom-tabs.tsx
+++ src/screens/bottom-tabs.tsx
@@ -1,6 +1,7 @@
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
 import { Home } from './home'
 import { History } from './history'
+import { Analytics } from './analytics'
 
 const BottomTabs = createBottomTabNavigator()
 
@@ -9,6 +10,7 @@ export const BottomTabsNavigator = () => {
     <BottomTabs.Navigator>
       <BottomTabs.Screen name="Home" component={Home} />
       <BottomTabs.Screen name="History" component={History} />
+      <BottomTabs.Screen name="Analytics" component={Analytics} />
     </BottomTabs.Navigator>
   )
 }
