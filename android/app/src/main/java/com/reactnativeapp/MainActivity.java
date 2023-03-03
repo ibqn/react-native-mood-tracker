ce0656167577e3742bca6aa144223ef58fd2fbd9
ibqn <evgen.ibqn@gmail.com>
2023-03-03 17:08:35 +0100

    add react navigation and bottom tabs

--- android/app/src/main/java/com/reactnativeapp/MainActivity.java
+++ android/app/src/main/java/com/reactnativeapp/MainActivity.java
@@ -5,6 +5,10 @@ import com.facebook.react.ReactActivityDelegate;
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint;
 import com.facebook.react.defaults.DefaultReactActivityDelegate;
 
+// required for react native navigation (react-native-screens)
+import android.os.Bundle;
+
+
 public class MainActivity extends ReactActivity {
 
   /**
@@ -14,6 +18,12 @@ public class MainActivity extends ReactActivity {
   @Override
   protected String getMainComponentName() {
     return "reactNativeApp";
+  }
+
+  // required for react native navigation (react-native-screens)
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(null);
   }
 
   /**
